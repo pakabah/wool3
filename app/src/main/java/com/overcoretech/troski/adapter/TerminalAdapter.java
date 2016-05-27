f56615136508e4620d809408358124c3ecc554e6
AKabah Paul Cudjoe <paulakabah@gmail.com>
2016-05-27 17:06:05 +0000

    terminal Adapter

--- app/src/main/java/com/overcoretech/troski/adapter/TerminalAdapter.java
+++ app/src/main/java/com/overcoretech/troski/adapter/TerminalAdapter.java
@@ -1,16 +1,20 @@
 package com.overcoretech.troski.adapter;
 
+import android.graphics.Color;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.overcoretech.troski.R;
 import com.overcoretech.troski.template.TerminalTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by pakabah on 27/05/16.
@@ -18,6 +22,12 @@ import java.util.List;
 public class TerminalAdapter extends RecyclerView.Adapter<TerminalAdapter.ViewHolder>{
 
     List<TerminalTemplate> terminalTemplates;
+
+    public TerminalAdapter(List<TerminalTemplate> terminalTemplates)
+    {
+        this.terminalTemplates = new ArrayList<>();
+        this.terminalTemplates.addAll(terminalTemplates);
+    }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -30,6 +40,10 @@ public class TerminalAdapter extends RecyclerView.Adapter<TerminalAdapter.ViewHo
         TerminalTemplate terminalTemplate = terminalTemplates.get(position);
         holder.textView.setText(terminalTemplate.TerminalName);
         holder.textView.setTag(terminalTemplate.TerminalId);
+
+        Random rnd = new Random();
+        int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+        holder.relativeLayout.setBackgroundColor(color);
 
         holder.cardView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,12 +62,14 @@ public class TerminalAdapter extends RecyclerView.Adapter<TerminalAdapter.ViewHo
     {
         CardView cardView;
         TextView textView;
+        RelativeLayout relativeLayout;
 
         public ViewHolder(View itemView) {
             super(itemView);
 
             cardView = (CardView) itemView.findViewById(R.id.terminalCard);
             textView = (TextView) itemView.findViewById(R.id.title);
+            relativeLayout = (RelativeLayout) itemView.findViewById(R.id.headerCol);
         }
     }
 }
--- app/src/main/java/com/overcoretech/troski/views/Terminals.java
+++ app/src/main/java/com/overcoretech/troski/views/Terminals.java
@@ -2,8 +2,15 @@ package com.overcoretech.troski.views;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.overcoretech.troski.R;
+import com.overcoretech.troski.adapter.TerminalAdapter;
+import com.overcoretech.troski.template.TerminalTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Terminals extends AppCompatActivity {
 
@@ -11,5 +18,21 @@ public class Terminals extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terminals);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycleTerminals);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        TerminalAdapter terminalAdapter = new TerminalAdapter(initializeData());
+        recyclerView.setAdapter(terminalAdapter);
+    }
+
+
+    public List<TerminalTemplate> initializeData()
+    {
+        List<TerminalTemplate> terminalTemplates = new ArrayList<>();
+        terminalTemplates.add(new TerminalTemplate());
+        terminalTemplates.add(new TerminalTemplate());
+
+        return terminalTemplates;
     }
 }
