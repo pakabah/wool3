4f5b07e5b9cf23076cca5681d246de8f531e9f08
AKabah Paul Cudjoe <paulakabah@gmail.com>
2016-05-28 09:54:36 +0000

    Crowd Layout

--- app/src/main/java/com/overcoretech/troski/views/Crowd.java
+++ app/src/main/java/com/overcoretech/troski/views/Crowd.java
@@ -1,7 +1,10 @@
 package com.overcoretech.troski.views;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.overcoretech.troski.R;
 
@@ -11,5 +14,24 @@ public class Crowd extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crowd);
+        initToolbar("Let's map Accra");
+    }
+
+    private void initToolbar(String title) {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(title);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        onBackPressed();
+        return super.onOptionsItemSelected(item);
     }
 }
